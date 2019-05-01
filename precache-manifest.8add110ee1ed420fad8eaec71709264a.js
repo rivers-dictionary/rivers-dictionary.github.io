ea465da0e645af30777811ccbafbabd08c1cdbeb
Lego Chiang <qas612820704@gmail.com>
2019-05-01 13:18:23 +0800

    Updates

---
+++ precache-manifest.8add110ee1ed420fad8eaec71709264a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f4e6afbc44552be7af3",
+    "url": "/static/css/main.a8399ad5.chunk.css"
+  },
+  {
+    "revision": "2f4e6afbc44552be7af3",
+    "url": "/static/js/main.f486b126.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "00bd6bddf850c24e8533",
+    "url": "/static/css/2.87128a5c.chunk.css"
+  },
+  {
+    "revision": "00bd6bddf850c24e8533",
+    "url": "/static/js/2.95ca9841.chunk.js"
+  },
+  {
+    "revision": "0d8822bc71f2a942b47434c484865383",
+    "url": "/index.html"
+  }
+];
